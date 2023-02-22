586928f1a374f0ee7ee2476cadb9f85862f0346d
K <koraljka.kosic@gmail.com>
2023-02-22 20:23:24 +0100

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+
+    val questionToBeAnswered = "The ultimate question of Life, the Universe and Everything"
+    val answer = 42
+    println(questionToBeAnswered)
+    println("Hello World!")
+
+
+}
